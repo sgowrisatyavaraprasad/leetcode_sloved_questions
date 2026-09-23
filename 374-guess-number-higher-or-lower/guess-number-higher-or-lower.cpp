acab5b75fca1e43ba257fc2964ad0d168f3c097d
gowri satya vara prasad saladi <sgowrisatyavaraprasadsgowri@gmail.com>
2026-09-23 21:05:39 +0530

    Time: 2 ms (56.53%) | Memory: 8 MB (6.31%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -0,0 +1,30 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        long long l = 1 , r = n;
+        while(l <= r){
+            long long m = int((l + r) / 2);
+            if(guess(m) == 0){
+                return m;
+            }
+            else if(guess(m) == 1){
+                l = m + 1;
+            }
+            else{
+                r = m - 1;
+            }
+            // cout << guess(m) << endl;
+            // return 0;
+        }
+        return 0;
+    }
+};
